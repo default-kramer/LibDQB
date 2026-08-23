a182d12b4ab9cf48786b54be24ce9cb9823a24bb
Ryan Kramer <default.kramer@gmail.com>
2026-08-23 10:40:25 -0500

    constant grid

---
+++ LibDQB/ConstantGrid.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibDQB;
+
+public sealed class ConstantGrid<T> : IReadOnlyGrid<T>
+{
+    public required Rect Bounds { get; init; }
+    public required T Value { get; init; }
+
+    public T Get(XZ xz) => Value;
+}
